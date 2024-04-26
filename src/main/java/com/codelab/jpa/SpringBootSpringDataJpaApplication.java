701fcb5a913e818c0325b61945052b17f6e95b42
maroti Jadhav <tech.maroti92@gmail.com>
2024-04-27 00:07:05 +0530

    second commit

--- src/main/java/com/codelab/jpa/SpringBootSpringDataJpaApplication.java
+++ src/main/java/com/codelab/jpa/SpringBootSpringDataJpaApplication.java
@@ -1,9 +1,25 @@
 package com.codelab.jpa;
 
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Employee-Service",
+		version = "1.0",
+		description = "Employees Information"
+),
+servers = @Server(
+		url = "http://localhost:8080",
+		description = "Employee Rest OpenApi"
+
+  )
+
+)
 public class SpringBootSpringDataJpaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/codelab/jpa/controller/EmployeeController.java
+++ src/main/java/com/codelab/jpa/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.codelab.jpa.controller;
 
 import com.codelab.jpa.model.Employee;
 import com.codelab.jpa.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,33 +11,59 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@Tag(name = "Employee-Service", description = "To perform operation on Employee-Service")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
+    @Operation(
+            summary = "POST operation on employee-service",
+            description = "It is used to save employee object in database"
+    )
     @PostMapping("/employee")
     public Employee saveEmployee(@RequestBody Employee employee) {
+
         return employeeService.saveEmployee(employee);
     }
 
+    @Operation(
+            summary = "GET operation on employee-service",
+            description = "It is used to retrieve employees object from database"
+    )
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
+
         return employeeService.fetchAllEmployees();
     }
 
+
+    @Operation(
+            summary = "GET operation on employee-service",
+            description = "It is used to retrieve employee object from database by using id"
+    )
     @GetMapping("/employee/{id}")
     public Employee getEmployeeById(@PathVariable("id") Long id) {
+
         return employeeService.getEmployeeById(id);
     }
 
+    @Operation(
+            summary = "PUT operation on employee-service",
+            description = "It is used to update employee object in database by using id"
+    )
     @PutMapping("/employee/{id}")
     public Employee updateEmployee(@PathVariable("id") Long id, @RequestBody Employee employee) {
         return employeeService.updateEmployeeById(id, employee);
     }
 
+    @Operation(
+            summary = "DELETE operation on employee-service",
+            description = "It is used to delete employee object from database by using id"
+    )
     @DeleteMapping("/employee/{id}")
     public String deleteEmployee(@PathVariable("id") Long id) {
+
         return employeeService.deleteDepartmentById(id);
     }
 
